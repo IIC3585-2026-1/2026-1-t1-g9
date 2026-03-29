8686b8791b145557d3efc1222293948283fb3dec
Ignacio Martinez <ignaciomartinez@uc.cl>
2026-03-29 00:17:48 -0300

    added grid demo

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 function switchDirection() {
     const container = document.querySelector('.flexbox-demo');
-    if (container.style.flexDirection === 'row') {
+    const text = document.getElementById('directionText');
+
+    const current = getComputedStyle(container).flexDirection;
+
+    if (current === 'row') {
         container.style.flexDirection = 'column';
+        text.textContent = 'flex-direction: column;';
     } else {
         container.style.flexDirection = 'row';
+        text.textContent = 'flex-direction: row;';
+    }
+}
+
+const gridContainer = document.querySelector('.grid-demo');
+
+let columns = 2;
+
+function updateGrid() {
+    gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    const text = document.getElementById('columnText');
+    text.textContent = `grid-template-columns: repeat(${columns}, 1fr);`;
+}
+
+function addColumn() {
+    if (columns < 4) {
+        columns++;
+        updateGrid();
+    }
+}
+
+function removeColumn() {
+    if (columns > 1) {
+        columns--;
+        updateGrid();
     }
 }
